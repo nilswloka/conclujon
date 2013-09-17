47377c63fda48e8960a7cfdda09069924f8c2d60
Nils Wloka <nils.wloka@codecentric.de>
2013-09-17 15:21:57 +0200

    Prepare for minor release

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
-(defproject conclujon "0.1.0"
+(defproject conclujon "0.1.1"
   :description "A framework for writing and executing active specifications in HTML."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/nilswloka/conclujon"
+  :scm {:name "git"
+        :url "https://github.com/nilswloka/conclujon"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [enlive "1.1.4"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
